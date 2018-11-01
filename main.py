ffe1808d415c59d7ca831b380627aaa7356f7c2f
Dmitry Grigorev <gridmi007@gmail.com>
2018-11-01 15:08:00 +0300

    bla

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
 
-token = ''
+token = '709661490:AAG6OK_phXyJ1E2_ALwK5HR0eylsTEjBd5A'
 REQUEST_KWARGS = {
-    
+    'proxy_url': 'socks5://vilunov.me:1488/'
 }
 
 
@@ -12,7 +12,7 @@ def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
 
 
-updater = Updater(token=token, request_kwargs=REQUEST_KWARGS)
+updater = Updater(token=token)
 dispatcher = updater.dispatcher
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
